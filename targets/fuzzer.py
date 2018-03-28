c49c1dbbeb4859efdbbad2f3da7e145e0b42f9f3
ryanmars <ryanmars@umich.edu>
2018-03-28 15:47:48 -0400

    Other test cases in comments

--- targets/fuzzer.py
+++ targets/fuzzer.py
@@ -3,20 +3,34 @@ import subprocess
 import os
 import base64
 import sys
-import random
+from random import randint
+from random import choice
 import string
 
 testkey = "Key"
 testint = "1"
 testdoub = "1.0"
-testchar = '0'
-testarray = "[1,1,1]"
+testchar = '1'
+teststr = "A"
+testarray = "[1]"
 
-while True:
+iter = 0
+
+while True and iter < 50:
 	testdoub = testdoub.replace('.','')
-	testdoub = testdoub + str(random.randint(0,99999)) + "." + testdoub
+	testdoub = testdoub + str(randint(0,99999)) + "." + testdoub
 	testobject = "{\"" + testkey + "\":\"" + testdoub + "\"}"
 
+	#testarrayleft = testarray.replace(']','')
+	#testarrayright = testarray.replace('[','')
+	#testarray = testarrayleft + "," + str(randint(0,9)) + "," + testarrayright
+	#testobject = "{\"" + testkey + "\":\"" + testarray + "\"}"
+
+	#testint = testint + str(randint(0,9)) + testint
+	#testobject = "{\"" + testkey + "\":\"" + testint + "\"}"
+
+	#teststr = teststr + choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) + teststr
+	#testobject = "{\"" + testkey + "\":\"" + teststr + "\"}"
 
 	child = subprocess.Popen("./jsonParser", stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 	_, stdErrOut = child.communicate(input = testobject)
@@ -24,3 +38,5 @@ while True:
 		print "Error: {}".format(child.returncode)
 		print base64.b64encode(testobject)
 		sys.exit(0)
+
+	iter = iter + 1

c51e4b76c53bb5fe92064793f9da797ba2b8355b
ryanmars <ryanmars@umich.edu>
2018-03-28 15:07:22 -0400

    fuzzer.py initial test finished

--- targets/fuzzer.py
+++ targets/fuzzer.py
@@ -3,35 +3,24 @@ import subprocess
 import os
 import base64
 import sys
+import random
+import string
 
-testCases = [
-    '''{"a":"b\\""}''',
-    '''{"a":"b"}''',
-    '''{"a":["b",1]}''',
-    '''{"asdf":"qwer","zxcv":[1,2,3,4],"fdsa":{"a":"b"}}''',
-    '''{"a":1.234,"b":"c"}''',
-    '''{"a":true,"b":false,"c":null}''',
-    '''{"a":-1}''',
-    '''{"a":"?"}''',
-    '''{"a":"this is mult"}''',
-    '''{"a mult":"t"}''',
-    '''{ "a":"t"}''',
-    '''{"a" :"t"}''',
-    '''{"a": "t"}''',
-    '''{"a":"t" }''',
-    '''{"a":1E-4}''',
-    '''{a}''',
-    '''"asdf"''',
-    '''{1:"a"}''',
-    '''{"a:1}''',
-    '''{"a":truetrue}''',
-    '''{"a":1234.123.123}''',
-    '''{"a":1E-4E-5}'''
-]
+testkey = "Key"
+testint = "1"
+testdoub = "1.0"
+testchar = '0'
+testarray = "[1,1,1]"
 
-for testcase in testCases :
-    child = subprocess.Popen("./jsonParser", stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    _, stdErrOut = child.communicate(input = testcase)
-    if child.returncode != 0 or stdErrOut != "" :
-        print "|%s|" % stdErrOut
-        sys.exit(0)
+while True:
+	testdoub = testdoub.replace('.','')
+	testdoub = testdoub + str(random.randint(0,99999)) + "." + testdoub
+	testobject = "{\"" + testkey + "\":\"" + testdoub + "\"}"
+
+
+	child = subprocess.Popen("./jsonParser", stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+	_, stdErrOut = child.communicate(input = testobject)
+	if child.returncode != 0:
+		print "Error: {}".format(child.returncode)
+		print base64.b64encode(testobject)
+		sys.exit(0)
